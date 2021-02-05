56eea212576e0dc0f6d36f1dec593a8e068ddd8f
ean <eanpraha@gmail.com>
2021-02-05 13:28:33 +0700

    success to build debug app

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import { name as appName } from './app.json';
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Router from '@/Router/'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = Router;
+class Main extends Component {
+    constructor(properties) {
+        super(properties);
+    }
+    render() {
+        return (
+            <NavigationContainer>
+                <App />
+            </NavigationContainer>
+        );
+    }
 
-AppRegistry.registerComponent(appName, () => App);
+}
+
+
+AppRegistry.registerComponent(appName, () => Main);
